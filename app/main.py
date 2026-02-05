48967203c613143235f081e3a2aa47324b2c2380
sabari vidhuran <santhiyasm15@gmail.com>
2026-02-05 15:11:35 +0530

    Demo run

---
+++ app/main.py
@@ -0,0 +1,11 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/")
+def root():
+    return {"messages":"Hello.."}
+
+@app.get("/health")
+def health_check():
+    return {"messages":"Health check"}
